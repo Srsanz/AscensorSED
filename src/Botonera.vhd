a3db4f119812b4b8bf4105cb00735dec799b5885
Can1l <a.canilm@alumnos.upm.es>
2023-11-30 17:03:18 +0100

    Update Botonera.vhd

--- src/Botonera.vhd
+++ src/Botonera.vhd
@@ -1,43 +1,24 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 30.11.2023 15:47:43
--- Design Name: 
--- Module Name: Botonera - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Botonera is
---  Port ( );
+    port (
+        b0, b1, b2, b3: in std_logic;
+        piso_deseado: out std_logic_vector(1 downto 0)
+    );
 end Botonera;
 
-architecture Behavioral of Botonera is
-
+architecture dataflow of Botonera is
+    signal botones: std_logic_vector(3 downto 0);
 begin
+botones <= b3 & b2 & b1 & b0;
+    with botones select
+        piso_deseado<= "00" when "0001",
+                       "01" when "0010",
+                       "10" when "0100",
+                       "11" when "1000",
+                       "ZZ" when others;
+end dataflow;
 
-
-end Behavioral;
