ee085ae0d1b9b4743f1d33acaae09da66c287024
Can1l <a.canilm@alumnos.upm.es>
2023-11-30 16:02:47 +0100

    Entidad_cabina
    cabina con entradas y salidas sin arch

---
+++ src/Botonera.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 30.11.2023 15:47:43
+-- Design Name: 
+-- Module Name: Botonera - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Botonera is
+--  Port ( );
+end Botonera;
+
+architecture Behavioral of Botonera is
+
+begin
+
+
+end Behavioral;
---
+++ src/Cabina.vhd
@@ -0,0 +1,31 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Cabina is
+    Port ( 
+        b0,b1,b2,b3,bemer: in std_logic;
+        abre_cierra, sube_baja: in std_logic_vector(1 downto 0);
+        clk: in std_logic;
+        an: in std_logic_vector(7 downto 0);
+        piso_destino, piso, abierto_cerrado: out std_logic_vector(1 downto 0);
+        emer: out std_logic;
+        led: out std_logic_vector(15 downto 0);
+        --led16,led17: out std_logic_vecor (2 downto 0);
+        led16_r,led16_g,led16_b,led17_r,led17_g,led17_b: out std_logic;
+        --segment: out std_logic_vector(7 donwto 0);
+         ca, cb, cc, cd, ce, cf, cg: out std_logic       
+        );
+end Cabina; 
+
+architecture Behavioral of Cabina is
+begin
+end architecture;
